00e5bd330a7bb53056f67c96d72bb72c0b6aa45b
Peter Bergström <peter.bergstrom@eniro.com>
2015-02-26 22:05:25 +0100

    Better for mobile

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 require_once 'classes.php';
 
-$template = new Template('templates/main.html');
 
 if (isset($_GET['song_id']) && $song = Song::get((int) $_GET['song_id']))
 {
-    $template->assign('search', '');
+    $template = new Template('templates/main_song.html');
     $chords = new Template('templates/chords.html');
     $chords->assign('chords', $song->getChords());
     $chords->assign('title', $song->name);
@@ -14,7 +13,8 @@ if (isset($_GET['song_id']) && $song = Song::get((int) $_GET['song_id']))
 }
 else
 {
-    $template->assign('l', isset($_GET['l']) ? $_GET['l'] : null);
+    $template = new Template('templates/main.html');
+    $template->assign('l', isset($_GET['l']) ? $_GET['l'] : '');
     $view = new SongList(Song::getList());
     $template->assign('middle', $view->render());
 }
